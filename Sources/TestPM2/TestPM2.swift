5ade42e1732bd109dc1458370e46f59328f932f7
Igor <ivanov.igor.it@gmail.com>
2023-01-13 13:49:37 +0300

    next

---
+++ Sources/TestPM2/TestPM2.swift
@@ -0,0 +1,6 @@
+public struct TestPM2 {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/TestPM2Tests/TestPM2Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import TestPM2
+
+final class TestPM2Tests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(TestPM2().text, "Hello, World!")
+    }
+}
